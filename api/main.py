bafd400684d6f13109a85b02b2dd889d7ef68dc1
leo9uo <leo.g9oo@outlook.com>
2024-07-03 12:19:08 +0200

    rename backend services

--- api/main.py
+++ api/main.py
@@ -2,9 +2,9 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import List, Optional
 import json
-from services.yfinance_utils import YFinanceUtils
-from services.sec_api_utils import SecApiUtils
-from services.finnhub_utils import FinnhubUtils
+from services.yfinance import YFinanceUtils
+from services.sec_api import SecApiUtils
+from services.finnhub import FinnhubUtils
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
